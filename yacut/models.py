807bd534db8af552e23cb987c49f91e8a5be1ca5
Maksim <Maxim-Zhadan-2002@yandex.ru>
2025-09-30 11:54:16 +0300

    Add URLMap model

--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,10 @@
-from datetime import datetime
+from sqlalchemy import func
 
 from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.Text, unique=True, nullable=False)
+    short = db.Column(db.Text, unique=True, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=func.now())
