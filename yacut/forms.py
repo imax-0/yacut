ac919f29a9ff0cbb26475ff6de0066c2bf7438ea
Maksim <Maxim-Zhadan-2002@yandex.ru>
2025-09-30 12:28:03 +0300

    Add download to disk form and add submit field in URLMapForm

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import URLField, StringField
+from flask_wtf.file import MultipleFileField
+from wtforms import URLField, StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 from yacut.constants import MIN_SHORT_LINK_ID_LENGTH, MAX_SHORT_LINK_ID_LENGTH
@@ -14,3 +15,9 @@ class URLMapForm(FlaskForm):
         "Ваш вариант короткой ссылки",
         validators=[Length(min=MIN_SHORT_LINK_ID_LENGTH, max=MAX_SHORT_LINK_ID_LENGTH)]
     )
+    submit = SubmitField('Создать')
+
+
+class DownloadToDiskForm(FlaskForm):
+    files = MultipleFileField()
+    submit = SubmitField('Загрузить')
